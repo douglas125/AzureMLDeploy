6b014e3897848eeede3bf5cdf433194e54cd7dea
douglas125 <douglas125@gmail.com>
2019-01-01 13:56:23 -0200

    Completed MLServer notebook and client side c# segmentation app

--- csharp/AzureImageModelDeployExample/AzureImageModelDeployExample/Form1.Designer.cs
+++ csharp/AzureImageModelDeployExample/AzureImageModelDeployExample/Form1.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.gbImgUpload = new System.Windows.Forms.GroupBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtResultsendpoint = new System.Windows.Forms.TextBox();
             this.txtPredictEndpoint = new System.Windows.Forms.TextBox();
             this.btnGetResults = new System.Windows.Forms.Button();
             this.btnSendPredict = new System.Windows.Forms.Button();
@@ -37,19 +37,27 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pbMask = new System.Windows.Forms.PictureBox();
             this.picImg = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbl_taskid = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkOverlay = new System.Windows.Forms.CheckBox();
+            this.chkLayers = new System.Windows.Forms.CheckedListBox();
             this.gbImgUpload.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMask)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picImg)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbImgUpload
             // 
             this.gbImgUpload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.gbImgUpload.Controls.Add(this.textBox2);
+            this.gbImgUpload.Controls.Add(this.lbl_taskid);
+            this.gbImgUpload.Controls.Add(this.label3);
+            this.gbImgUpload.Controls.Add(this.txtResultsendpoint);
             this.gbImgUpload.Controls.Add(this.txtPredictEndpoint);
             this.gbImgUpload.Controls.Add(this.btnGetResults);
             this.gbImgUpload.Controls.Add(this.btnSendPredict);
@@ -58,19 +66,21 @@
             this.gbImgUpload.Controls.Add(this.label1);
             this.gbImgUpload.Location = new System.Drawing.Point(12, 12);
             this.gbImgUpload.Name = "gbImgUpload";
-            this.gbImgUpload.Size = new System.Drawing.Size(1047, 135);
+            this.gbImgUpload.Size = new System.Drawing.Size(1237, 135);
             this.gbImgUpload.TabIndex = 0;
             this.gbImgUpload.TabStop = false;
             this.gbImgUpload.Text = "Select image to upload";
             // 
-            // textBox2
+            // txtResultsendpoint
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtResultsendpoint.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Location = new System.Drawing.Point(99, 96);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(942, 20);
-            this.textBox2.TabIndex = 2;
+            this.txtResultsendpoint.Location = new System.Drawing.Point(99, 96);
+            this.txtResultsendpoint.Name = "txtResultsendpoint";
+            this.txtResultsendpoint.Size = new System.Drawing.Size(1132, 20);
+            this.txtResultsendpoint.TabIndex = 2;
+            this.txtResultsendpoint.Text = "https://blobstorefuncteste.azurewebsites.net/api/getresult_v0?code=i29ppJFs7cfv/p" +
+    "QgfF4KD40YsWKbLYvgI9JnRwPqvhUG1mCUYixdwQ==";
             // 
             // txtPredictEndpoint
             // 
@@ -78,27 +88,29 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtPredictEndpoint.Location = new System.Drawing.Point(99, 73);
             this.txtPredictEndpoint.Name = "txtPredictEndpoint";
-            this.txtPredictEndpoint.Size = new System.Drawing.Size(942, 20);
+            this.txtPredictEndpoint.Size = new System.Drawing.Size(1132, 20);
             this.txtPredictEndpoint.TabIndex = 2;
             this.txtPredictEndpoint.Text = "https://blobstorefuncteste.azurewebsites.net/api/predict_v0?code=XHeGapCuyxD7N3ft" +
     "oOOPUTCZY0XHMC0XtdfbzjG0/GcWLFoRCYlk9w==";
             // 
             // btnGetResults
             // 
-            this.btnGetResults.Enabled = false;
-            this.btnGetResults.Location = new System.Drawing.Point(273, 19);
+            this.btnGetResults.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetResults.Location = new System.Drawing.Point(353, 19);
             this.btnGetResults.Name = "btnGetResults";
-            this.btnGetResults.Size = new System.Drawing.Size(126, 43);
+            this.btnGetResults.Size = new System.Drawing.Size(192, 43);
             this.btnGetResults.TabIndex = 1;
             this.btnGetResults.Text = "Retrieve results";
             this.btnGetResults.UseVisualStyleBackColor = true;
+            this.btnGetResults.Click += new System.EventHandler(this.btnGetResults_Click);
             // 
             // btnSendPredict
             // 
             this.btnSendPredict.Enabled = false;
-            this.btnSendPredict.Location = new System.Drawing.Point(141, 19);
+            this.btnSendPredict.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSendPredict.Location = new System.Drawing.Point(163, 19);
             this.btnSendPredict.Name = "btnSendPredict";
-            this.btnSendPredict.Size = new System.Drawing.Size(126, 43);
+            this.btnSendPredict.Size = new System.Drawing.Size(184, 43);
             this.btnSendPredict.TabIndex = 1;
             this.btnSendPredict.Text = "Send for prediction";
             this.btnSendPredict.UseVisualStyleBackColor = true;
@@ -106,9 +118,10 @@
             // 
             // btnLoadImage
             // 
+            this.btnLoadImage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLoadImage.Location = new System.Drawing.Point(9, 19);
             this.btnLoadImage.Name = "btnLoadImage";
-            this.btnLoadImage.Size = new System.Drawing.Size(126, 43);
+            this.btnLoadImage.Size = new System.Drawing.Size(148, 43);
             this.btnLoadImage.TabIndex = 1;
             this.btnLoadImage.Text = "Load Image...";
             this.btnLoadImage.UseVisualStyleBackColor = true;
@@ -134,39 +147,91 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.pbMask);
             this.groupBox2.Controls.Add(this.picImg);
-            this.groupBox2.Location = new System.Drawing.Point(12, 153);
+            this.groupBox2.Location = new System.Drawing.Point(200, 153);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1047, 538);
+            this.groupBox2.Size = new System.Drawing.Size(1049, 538);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Image";
             // 
+            // pbMask
+            // 
+            this.pbMask.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.pbMask.Location = new System.Drawing.Point(527, 19);
+            this.pbMask.Name = "pbMask";
+            this.pbMask.Size = new System.Drawing.Size(512, 512);
+            this.pbMask.TabIndex = 0;
+            this.pbMask.TabStop = false;
+            // 
             // picImg
             // 
+            this.picImg.Cursor = System.Windows.Forms.Cursors.Cross;
             this.picImg.Location = new System.Drawing.Point(9, 19);
             this.picImg.Name = "picImg";
             this.picImg.Size = new System.Drawing.Size(512, 512);
             this.picImg.TabIndex = 0;
             this.picImg.TabStop = false;
             // 
-            // pictureBox1
+            // label3
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(527, 19);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(551, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Task id:";
+            // 
+            // lbl_taskid
+            // 
+            this.lbl_taskid.AutoSize = true;
+            this.lbl_taskid.Location = new System.Drawing.Point(602, 35);
+            this.lbl_taskid.Name = "lbl_taskid";
+            this.lbl_taskid.Size = new System.Drawing.Size(242, 13);
+            this.lbl_taskid.TabIndex = 3;
+            this.lbl_taskid.Text = "59729da0-affb-45ec-b867-c9544f935946.payload";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.chkLayers);
+            this.groupBox1.Controls.Add(this.chkOverlay);
+            this.groupBox1.Location = new System.Drawing.Point(12, 154);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(182, 537);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Categories";
+            // 
+            // chkOverlay
+            // 
+            this.chkOverlay.AutoSize = true;
+            this.chkOverlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkOverlay.Location = new System.Drawing.Point(6, 30);
+            this.chkOverlay.Name = "chkOverlay";
+            this.chkOverlay.Size = new System.Drawing.Size(130, 24);
+            this.chkOverlay.TabIndex = 0;
+            this.chkOverlay.Text = "Overlay masks";
+            this.chkOverlay.UseVisualStyleBackColor = true;
+            this.chkOverlay.CheckedChanged += new System.EventHandler(this.chkOverlay_CheckedChanged);
+            // 
+            // chkLayers
+            // 
+            this.chkLayers.CheckOnClick = true;
+            this.chkLayers.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkLayers.FormattingEnabled = true;
+            this.chkLayers.Location = new System.Drawing.Point(9, 71);
+            this.chkLayers.Name = "chkLayers";
+            this.chkLayers.Size = new System.Drawing.Size(167, 441);
+            this.chkLayers.TabIndex = 1;
+            this.chkLayers.SelectedIndexChanged += new System.EventHandler(this.chkLayers_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1071, 703);
+            this.ClientSize = new System.Drawing.Size(1261, 703);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.gbImgUpload);
             this.Name = "Form1";
@@ -175,8 +240,10 @@
             this.gbImgUpload.ResumeLayout(false);
             this.gbImgUpload.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMask)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.picImg)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -184,7 +251,7 @@
         #endregion
 
         private System.Windows.Forms.GroupBox gbImgUpload;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtResultsendpoint;
         private System.Windows.Forms.TextBox txtPredictEndpoint;
         private System.Windows.Forms.Button btnGetResults;
         private System.Windows.Forms.Button btnSendPredict;
@@ -193,7 +260,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.PictureBox picImg;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbMask;
+        private System.Windows.Forms.Label lbl_taskid;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chkOverlay;
+        private System.Windows.Forms.CheckedListBox chkLayers;
     }
 }
 
--- csharp/AzureImageModelDeployExample/AzureImageModelDeployExample/Form1.cs
+++ csharp/AzureImageModelDeployExample/AzureImageModelDeployExample/Form1.cs
@@ -8,6 +8,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,6 +23,8 @@ namespace AzureImageModelDeployExample
         }
 
         Bitmap bmp;
+        Bitmap mask;
+
         private void btnLoadImage_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
@@ -30,6 +33,7 @@ namespace AzureImageModelDeployExample
             {
                 bmp = new Bitmap(ofd.FileName);
                 bmp = ResizeImage(bmp, 512, 512); //we know that we want inference on this size
+
                 picImg.Image = bmp;
                 btnSendPredict.Enabled = true;
             }
@@ -38,18 +42,78 @@ namespace AzureImageModelDeployExample
         private static readonly HttpClient client = new HttpClient();
         private async void btnSendPredict_Click(object sender, EventArgs e)
         {
+            lbl_taskid.Text = "";
             string imgBase64 = bmp2base64(bmp);
 
             string content = "{'payload': '" + imgBase64 + "'}";
             var response = await client.PostAsync(txtPredictEndpoint.Text, new StringContent(content, Encoding.UTF8, "application/json"));
             var responseString = await response.Content.ReadAsStringAsync();
 
-            gbImgUpload.Text = "task id:"+ responseString;
+            lbl_taskid.Text = responseString;
 
             btnGetResults.Enabled = true;
         }
 
+
+        private async void btnGetResults_Click(object sender, EventArgs e)
+        {
+            string content = "{'task_id': '" + lbl_taskid.Text + "'}";
+            
+            var response = await client.PostAsync(txtResultsendpoint.Text, new StringContent(content, Encoding.UTF8, "application/json"));
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            if (responseString != "dGFza19pZCBub3QgZm91bmQh")
+            {
+                byte[] imageData = Convert.FromBase64String(responseString);
+                using (var ms = new MemoryStream(imageData))
+                {
+                    mask = new Bitmap(ms);
+                }
+
+                drawMask();
+            }
+            else MessageBox.Show("Task result not found");
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach(string s in label_names)
+            {
+                chkLayers.Items.Add(s);
+            }
+            for (int i = 0; i < chkLayers.Items.Count; i++) chkLayers.SetItemChecked(i, true);
+            resetColors();
+        }
+        void resetColors()
+        {
+            label_colors.Clear();
+            label_colors.Add(Color.Black);
+            Random rnd = new Random(10);
+            for (int i = 1; i < label_names.Count; i++)
+            {
+                Color c = Color.FromArgb(rnd.Next(255), rnd.Next(255), rnd.Next(255));
+                label_colors.Add(c);
+            }
+        }
+
+        private void chkLayers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            drawMask();
+        }
+
+        private void chkOverlay_CheckedChanged(object sender, EventArgs e)
+        {
+            drawMask();
+        }
+
         #region Deal with images
+        /// <summary>Label names, same convention as segmentation model</summary>
+        List<string> label_names = new List<string>() {"background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",
+                                                        "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
+                                                        "person", "pottedplant", "sheep", "sofa", "train", "tv" };
+        List<Color> label_colors = new List<Color>();
         string bmp2base64(Bitmap bitmap)
         {
             using (var ms = new MemoryStream())
@@ -90,14 +154,154 @@ namespace AzureImageModelDeployExample
 
             return destImage;
         }
+
+        //draw mask to image
+        void drawMask()
+        {
+            if (mask == null) return;
+
+            Bitmap maskdraw = new Bitmap(mask.Width, mask.Height);
+            Graphics g = Graphics.FromImage(maskdraw);
+            g.DrawImage(mask, 0, 0);
+            int[,] classes = RemakeMask(maskdraw);
+            pbMask.Image = maskdraw;
+
+            if (bmp != null && chkOverlay.Checked)
+            {
+                Bitmap bmpover = (Bitmap)bmp.Clone();
+                DrawOverlay(bmpover, classes);
+
+                picImg.Image = bmpover;
+            }
+            else picImg.Image = bmp;
+        }
+
+        public int[,] RemakeMask(Bitmap processedBitmap)
+        {
+            int W = processedBitmap.Width;
+            BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+
+            int[,] vals = new int[W, processedBitmap.Height];
+
+
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+            int byteCount = bitmapData.Stride * processedBitmap.Height;
+            byte[] pixels = new byte[byteCount];
+            IntPtr ptrFirstPixel = bitmapData.Scan0;
+            Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+
+            int xx, yy;
+            yy = 0;
+
+            for (int y = 0; y < heightInPixels; y++)
+            {
+                int currentLine = y * bitmapData.Stride;
+                xx = 0;
+                for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                {
+                    int oldBlue = pixels[currentLine + x];
+                    int oldGreen = pixels[currentLine + x + 1];
+                    int oldRed = pixels[currentLine + x + 2];
+
+                    //replace with label_colors
+                    byte  r = label_colors[oldBlue].R;
+                    byte g = label_colors[oldBlue].G;
+                    byte b = label_colors[oldBlue].B;
+
+                    if (chkLayers.GetItemChecked(oldBlue))
+                    {
+                        pixels[currentLine + x] = b;
+                        pixels[currentLine + x + 1] = g;
+                        pixels[currentLine + x + 2] = r;
+                        pixels[currentLine + x + 3] = 255;
+                    }
+                    else
+                    {
+                        pixels[currentLine + x] = 0;
+                        pixels[currentLine + x + 1] = 0;
+                        pixels[currentLine + x + 2] = 0;
+                        pixels[currentLine + x + 3] = 255;
+                    }
+                    vals[xx, yy] = oldBlue;
+
+                    xx++;
+                }
+                yy++;
+            }
+
+            // copy modified bytes back
+            Marshal.Copy(pixels, 0, ptrFirstPixel, pixels.Length);
+            processedBitmap.UnlockBits(bitmapData);
+
+            return vals;
+        }
+
+
+        public void DrawOverlay(Bitmap processedBitmap, int[,] vals)
+        {
+            int W = processedBitmap.Width;
+            BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+            int byteCount = bitmapData.Stride * processedBitmap.Height;
+            byte[] pixels = new byte[byteCount];
+            IntPtr ptrFirstPixel = bitmapData.Scan0;
+            Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+
+            int xx, yy;
+            yy = 0;
+
+            for (int y = 0; y < heightInPixels; y++)
+            {
+                int currentLine = y * bitmapData.Stride;
+                xx = 0;
+                for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                {
+                    int oldBlue = pixels[currentLine + x];
+                    int oldGreen = pixels[currentLine + x + 1];
+                    int oldRed = pixels[currentLine + x + 2];
+
+                    //merge with label_colors
+                    byte r = label_colors[vals[xx,yy]].R;
+                    byte g = label_colors[vals[xx, yy]].G;
+                    byte b = label_colors[vals[xx, yy]].B;
+
+                    float maskIntens = 0.5f;
+                    byte avg = (byte)(  (1-maskIntens) * (oldBlue + oldGreen + oldRed) / 3.0f);
+                    r = (byte)(maskIntens * r);
+                    g = (byte)(maskIntens * g);
+                    b = (byte)(maskIntens * b);
+
+                    if (chkLayers.GetItemChecked(vals[xx,yy]))
+                    {
+                        pixels[currentLine + x] = (byte)(avg + b);
+                        pixels[currentLine + x + 1] = (byte)(avg + g);
+                        pixels[currentLine + x + 2] = (byte)(avg + r);
+                        pixels[currentLine + x + 3] = 255;
+                    }
+                    else
+                    {
+                        pixels[currentLine + x] = avg;
+                        pixels[currentLine + x + 1] = avg;
+                        pixels[currentLine + x + 2] = avg;
+                        pixels[currentLine + x + 3] = 255;
+                    }
+
+                    xx++;
+                }
+                yy++;
+            }
+
+            // copy modified bytes back
+            Marshal.Copy(pixels, 0, ptrFirstPixel, pixels.Length);
+            processedBitmap.UnlockBits(bitmapData);
+        }
+
         #endregion
 
-        /// <summary>Label names, same convention as segmentation model</summary>
-        List<string> label_names = new List<string>() {"background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus",
-                                                        "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike",
-                                                        "person", "pottedplant", "sheep", "sofa", "train", "tv" };
-        private void Form1_Load(object sender, EventArgs e)
-        {
-        }
     }
 }
